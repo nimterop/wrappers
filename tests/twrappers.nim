60f608a833f5e5ef6ae4937e6f7a5d72a5b5813e
Timothee Cour <timothee.cour2@gmail.com>
2019-01-31 15:43:28 -0800

    wrappers binary + tests that runs all wrappers

--- tests/twrappers.nim
+++ tests/twrappers.nim
@@ -1,6 +1,23 @@
-import unittest
+import std/[os,unittest,strformat]
 
-import wrapperspkg/submodule
+import wrapperspkg/[paths]
 
-test "placeholder test":
-  check getWelcomeMessage() == "Hello, World!"
+proc runCmdCheck(cmd: string) =
+  ## calls `check`, ie on error, fail after all commands are run instead of immediately
+  echo (runCmdCheck: cmd)
+  let ret = execShellCmd(cmd)
+  if ret != 0:
+    echo "runCmdCheck failed: " & $(ret: ret, cmd: cmd)
+    check false
+
+test "wrappers exe":
+  runCmdCheck &"nim c -r {repoSrcDir()}/wrappers.nim -h"
+
+test "import wrappers":
+  #[
+  An alternative is to import wrappers directly without
+  running shell commands
+  pros: simpler, faster
+  cons: can't run the rest if some dependency for 1 test is missing
+  ]#
+  runCmdCheck &"nim c -r {wrappersDir()}/soloud/demo.nim -h"

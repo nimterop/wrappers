b73b5c0212f8ba73c164b63159ba5e808229ec34
Timothee Cour <timothee.cour2@gmail.com>
2019-01-31 16:49:44 -0800

    fix CI

--- src/wrappers.nim
+++ src/wrappers.nim
@@ -1,18 +1,31 @@
 #[
-Tool to help manage wrappers, eg:
-* upgrade wrappers
-* generate individual nimble pacakges
+TODO:
+* helpers to upgrade wrappers when new upstream library is released
+* mabye: generate individual nimble packages (1 per wrapper)
 ]#
 
 import std/[os]
 import wrapperspkg/paths
 
-proc main() =
-  var wrappers: seq[string]
-  for kind, path in walkDir(formulasDir(), relative = true):
-    wrappers.add path
-  echo wrappers
+type Action = enum
+  list,
+
+proc main(action = list) =
+  ##[
+  Tool to help manage wrappers, for now:
+  * list all wrappers
+  ]##
+  case action
+  of list:
+    var wrappers: seq[string]
+    for kind, path in walkDir(formulasDir(), relative = true):
+      wrappers.add path
+    echo wrappers
 
 when isMainModule:
   import cligen
-  dispatch(main)
+  var ret: seq[string]
+  for a in Action: ret.add $a
+  # TODO: make this work
+  # dispatch(main, help = {"action": $ret})
+  dispatch(main, help = {"action": "see wrappers.Action"})

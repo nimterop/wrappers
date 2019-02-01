ce21908e7d48eabddb1d53f14670a56b19077c80
Timothee Cour <timothee.cour2@gmail.com>
2019-01-31 17:11:41 -0800

    skip soloud test on ubuntu

--- tests/twrappers.nim
+++ tests/twrappers.nim
@@ -2,6 +2,9 @@ import std/[os,unittest,strformat]
 
 import wrapperspkg/[paths]
 
+proc warnSkipped(cmd: string) =
+  echo "CAUTION: test skipped: " & cmd
+
 proc runCmdCheck(cmd: string) =
   ## calls `check`, ie on error, fail after all commands are run instead of immediately
   echo (runCmdCheck: cmd)
@@ -20,4 +23,17 @@ test "import wrappers":
   pros: simpler, faster
   cons: can't run the rest if some dependency for 1 test is missing
   ]#
-  runCmdCheck &"nim c -r {formulasDir()}/soloud/demo.nim -h"
+
+  let cmd = &"nim c -r {formulasDir()}/soloud/demo.nim -h"
+  if defined(OSX) or defined(Windows) or not existsEnv("TRAVIS"):
+    runCmdCheck cmd
+  else:
+    #[
+    travis ubuntu fails:
+Error: execution of an external compiler program 'gcc -c  -w -I/home/travis/.nimble/pkgs/build/soloud/include -DWITH_OSS  -I/home/travis/.choosenim/toolchains/nim-0.19.2/lib -o /home/travis/.cache/nim/demo_d/soloud_wav.cpp.o /home/travis/.nimble/pkgs/build/soloud/src/audiosource/wav/soloud_wav.cpp' failed with exit code: 1
+In file included from /home/travis/.nimble/pkgs/build/soloud/src/audiosource/wav/soloud_wav.cpp:34:0:
+/home/travis/.nimble/pkgs/build/soloud/src/audiosource/wav/dr_flac.h:145:43: error: missing binary operator before token "("
+ #elif (defined(__clang__) && __has_feature(attribute_deprecated))
+                                           ^
+    ]#
+    warnSkipped cmd
